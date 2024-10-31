fac2955de08f958fcfcbd1c0f2bf3329e33ce297
Elias Luhr <e.luhr@herzogkommunikation.de>
2024-10-31 14:20:10 +0100

    plugin options and default value

--- src/widgets/JediEditor.php
+++ src/widgets/JediEditor.php
@@ -7,6 +7,7 @@ use yii\base\InvalidConfigException;
 use yii\helpers\Html;
 use yii\helpers\Inflector;
 use yii\helpers\Json;
+use yii\web\JsExpression;
 use yii\widgets\InputWidget;
 
 class JediEditor extends InputWidget
@@ -17,16 +18,36 @@ class JediEditor extends InputWidget
      */
     public array $containerOptions = [];
 
-    public array $schema;
+    /**
+     * An json that contains the schema to build the form. Values can be given as array, string or stdClass
+     */
+    protected array $_schema;
+
+    /**
+     * Options to be passed to the jedi.
+     * List of valid options can be found here:
+     * https://github.com/germanbisurgi/jedi?tab=readme-ov-file#options
+     */
+    public array $pluginOptions = [];
 
     /**
      * @inheritdoc
+     * @throw yii\base\InvalidConfigException if some config is not as expected
      */
     public function init()
     {
         parent::init();
 
-        if (!isset($this->schema)) {
+        // If schema is set in plugin options use it from there
+        // TODO: Do we want that?
+        if (isset($this->pluginOptions['schema'])) {
+            $this->setSchema($this->pluginOptions['schema']);
+            unset($this->pluginOptions['schema']);
+        }
+
+        $this->ensurePluginOptions();
+
+        if (!isset($this->_schema)) {
             throw new InvalidConfigException("Property 'schema' must be specified.");
         }
 
@@ -70,29 +91,80 @@ class JediEditor extends InputWidget
     {
         JediAsset::register($this->view);
 
+        // Setup variables for later use
         $containerId = $this->containerOptions['id'];
         $inputId = $this->hasModel() ? Html::getInputId($this->model, $this->attribute) : $this->options['id'];
         $inputName = $this->hasModel() ? Html::getInputName($this->model, $this->attribute) : $this->name;
         $id = Inflector::slug($inputId, '');
 
-        $schema = Json::htmlEncode($this->schema);
+        // Escape json for config if needed
+        $schema = Json::htmlEncode($this->_schema);
+        $pluginOptions = Json::htmlEncode($this->pluginOptions);
 
+        // Init editor
         $this->view->registerJs(<<<JS
 const initEditor$id = () => {
-    const editorOptions = {
+    const defaultOptions = {
         container: document.getElementById('$containerId'),
-        theme: new Jedi.ThemeBootstrap3(),
+        // theme: new Jedi.ThemeBootstrap3(),
         schema: $schema,
         hiddenInputAttributes: {
             'name': '$inputName',
             'id': '$inputId'
         }
     }
+    
+    const customOptions = $pluginOptions
+    
+    const editorOptions = {...defaultOptions, ...customOptions}
+    
     const editor = new Jedi.Create(editorOptions) 
 }
 
 initEditor$id()
 JS
         );
+    }
+
+
+    /**
+     * Convert schema to json array if given as string or stdClass
+     */
+    public function setSchema(array|\stdClass|string $schema): void
+    {
+        if ($schema instanceof \stdClass) {
+            $schema = Json::encode($schema);
+            // Now that the value is a string, it is converted to an array in the next condition.
+        }
+
+        if (is_string($schema)) {
+            $schema = Json::decode($schema);
+        }
+
+        $this->_schema = $schema;
+    }
+
+    /**
+     * Reset all plugin options that should not be overwritten
+     */
+    protected function ensurePluginOptions(): void
+    {
+        unset($this->pluginOptions['container']);
+        unset($this->pluginOptions['hiddenInputAttributes']['name']);
+        unset($this->pluginOptions['hiddenInputAttributes']['id']);
+
+        // Set default theme if not set
+        if (!isset($this->pluginOptions['theme'])) {
+            $this->pluginOptions['theme'] = new JsExpression('new Jedi.Theme()');
+        }
+
+        // Set default value
+        if ($this->hasModel()) {
+            $data = $this->model->{$this->attribute};
+        } else {
+            $data = $this->value;
+        }
+        
+        $this->pluginOptions['data'] = new JsExpression($data);
     }
 }
